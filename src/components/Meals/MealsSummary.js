fc3a00a1d25dafe6cf22581a464836957cc49f8e
Kevin Pulley <skippy1025@gmail.com>
2021-06-01 20:40:32 -0500

    added MealSummary.js and css file

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealsSummary = () => {
+    return (
+   <section>
+       <h2>Delicious Food, Delivered To You</h2>
+       <p></p>
+   </section>
+    );
+};
+
+export default MealsSummary;
