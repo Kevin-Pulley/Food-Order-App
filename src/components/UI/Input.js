c131609cea88ab73cb447670c8ff106ecd3b6a7d
Kevin Pulley <skippy1025@gmail.com>
2021-06-21 19:37:33 -0500

    started shopping cart component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,9 +3,9 @@ import classes from './Input.module.css'
 const Input = props => {
     return (
         <div className={classes.input}>
-            <label htmlFor={props.input.id}>{props.label }</label>
+            <label htmlFor={props.input.id}>{props.label}</label>
             <input {...props.input} />
-            <input type="text" />
+
         </div>
     )
 }
