d8501f43a82e37dad9998d53ec0cb6faf590001b
Kevin Pulley <skippy1025@gmail.com>
2021-10-27 20:11:04 -0500

    sent user data and orders to firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -23,6 +23,16 @@ const Cart = (props) => {
     const orderHandler = () => {
         setIsCheckout(true);
     }
+
+    const submitOrderHandler = (userData) => {
+        fetch('https://react-my-burger-61ae3-default-rtdb.firebaseio.com/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        });
+    }
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => (
@@ -54,8 +64,8 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout onCancel={props.onClose}/>}
-            { !isCheckout && modalActions}
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
+            {!isCheckout && modalActions}
 
         </Modal>
     );
